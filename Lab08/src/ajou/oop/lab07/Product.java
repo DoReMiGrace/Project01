d54c5e7a37950d3ca5a31ffd3d0fb79202e78d52
Grace <19_____@naver.com>
2017-04-27 11:22:57 +0900

    commit product by eunhaeh

---
+++ Lab08/src/ajou/oop/lab07/Product.java
@@ -0,0 +1,17 @@
+package ajou.oop.lab07;
+
+public abstract class Product {
+
+	private double price;
+	public abstract void Product();
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	public String getName(String name) {
+		return name;
+	}
+}
