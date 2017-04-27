81c4cde70ab1f8706f4b21c6eb43774288c62560
lagunerio <lagunerio@gmail.com>
2017-04-27 11:35:58 +0900

    make class SmartTV

--- Lab08/src/ajou/oop/lab07/SmartTV.java
+++ Lab08/src/ajou/oop/lab07/SmartTV.java
@@ -1,11 +1,36 @@
 package ajou.oop.lab07;
 
 public class SmartTV extends Product {
-
-	@Override
-	public void Product() {
-		// TODO Auto-generated method stub
-
+	
+	int channel = 0;
+	
+	public SmartTV() {
 	}
 
+	public boolean isConnected(){
+		boolean tmp_b = true;
+		
+		return tmp_b;
+	}
+	
+	public double maxSpeed(){
+		double tmp_d = 0;
+		
+		return tmp_d;
+	}
+	
+	@Override
+	public String getName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getChannel() {
+		return channel;
+	}
+
+	public void setChannel(int channel) {
+		this.channel = channel;
+	}
+	
 }
